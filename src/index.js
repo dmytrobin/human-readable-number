3ae57371a43de0ddf9a9c9584b52da1fe5ca7a31
dmytrobin <dmytro.binyk@gmail.com>
2022-01-04 17:37:47 +0200

    feat: move logic to function

--- src/index.js
+++ src/index.js
@@ -1,39 +1,54 @@
-module.exports = function toReadable (number) {
-  let result, strNumber, strNumberLenght, secondDigit;
-
+function ones(number){
+  let numberTostring;
   var ones = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+  numberTostring = ones[number];
+  return numberTostring;
+  }
+  
+  function teens(number){
+  let numberTostring;
   var  teens = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen', 'nineteen'];
-  var  tens = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-  var hundreds = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+  numberTostring = teens[number-10];
+  return numberTostring;
+  }
+  
+  function tens(number){
+  let numberTostring, strNumber;
+    var  tens = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+     
+    strNumber = number.toString();
+     if (strNumber[1]*1 === 0 ){
+     numberTostring = tens[strNumber[0]*1-1];
+   } else {  numberTostring = tens[strNumber[0]*1-1] + '\u00A0' + ones(strNumber[1]) ;
+   }
+    
+  return numberTostring;
+  }
+  
+  function hundreds(){
+  
+  }
+  
+  function toReadable (number) {
+    let result;
+    var hundreds = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+  //strNumberLenght = strNumber.length;
+  if (0 <= number && number < 10){
+  result = ones(number);
+  } else if (10 <= number && number < 20){
+  result = teens(number);
+  } else if (20 <= number && number < 100){
+   result = tens(number);
+   } else if   (100 <= number && number < 1000){
    
-
-//strNumberLenght = strNumber.length;
-
-if (0 <= number && number < 10){
-result = ones[number];
-} else if (10 <= number && number < 20){
-result = teens[number-10];
-} else if (20 <= number && number < 100){
-strNumber = number.toString();
- if (strNumber[1]*1 === 0 ){
- 	result = tens[strNumber[0]*1-1];
- } else {  result = tens[strNumber[0]*1-1] + '\u00A0' + ones[strNumber[1]*1] ;
- }
-	
+   
+   }
+   
+     return result; 
+   }//toReadable
   
-}
-  
-  
-   return result;
-  
- }
- /*
-
- 22
-
- */
- let result;
- let number = 51;
- result = toReadable(number);
- 
-   console.log(result);
+   let result;
+   let number = 67;
+   result = toReadable(number);
+   
+  console.log(result);
