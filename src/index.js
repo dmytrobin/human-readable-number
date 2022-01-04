c0a8ac36fa3160fd3ad422b1089094e605695c92
dmytrobin <dmytro.binyk@gmail.com>
2022-01-04 19:53:03 +0200

    feat: complete code

--- src/index.js
+++ src/index.js
@@ -13,25 +13,36 @@ function ones(number){
   }
   
   function tens(number){
+  console.log(number);
   let numberTostring, strNumber;
     var  tens = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-     
+     let strNumberLenght, secondDigit;
+    
     strNumber = number.toString();
+   // console.log(strNumber);
      if (strNumber[1]*1 === 0 ){
+     
      numberTostring = tens[strNumber[0]*1-1];
-   } else {  numberTostring = tens[strNumber[0]*1-1] + '\u00A0' + ones(strNumber[1]) ;
+   } else { numberTostring = tens[strNumber[0]*1-1] + '\u00A0' + ones(strNumber[1]) ;
    }
     
   return numberTostring;
   }
   
-  function hundreds(){
+  function hundreds(number){
+   let numberTostring, strNumber, dd, d, zeroZero;
+   d = Math.floor(number/100);
+   dd = number - (Math.floor(number/100))*100;
   
+   dd  === 0? zeroZero = '' : zeroZero=tens(dd);
+   numberTostring = ones(d) + '\u00A0' + 'hundred' + '\u00A0' + zeroZero;
+  
+   return numberTostring;
   }
   
   function toReadable (number) {
     let result;
-    var hundreds = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+   
   //strNumberLenght = strNumber.length;
   if (0 <= number && number < 10){
   result = ones(number);
@@ -40,15 +51,14 @@ function ones(number){
   } else if (20 <= number && number < 100){
    result = tens(number);
    } else if   (100 <= number && number < 1000){
-   
-   
+   result = hundreds(number);
    }
    
      return result; 
    }//toReadable
   
    let result;
-   let number = 67;
-   result = toReadable(number);
+   let number = 965;
+   //result = toReadable(number);
    
   console.log(result);
