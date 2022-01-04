32ee6d50a086c21ba38ed46edafb7a2916953648
dmytrobin <dmytro.binyk@gmail.com>
2022-01-04 12:31:44 +0200

    feat: add if statements

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 module.exports = function toReadable (number) {
   let result;
+  let strNumber;
+  let strNumberLenght;
   var ones = ['zero','one','two','three','four','five','six','seven','eight','nine'];
- 
   var  teens = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen', 'nineteen'];
   var  tens = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
   var hundreds = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
    
-   console.log(singleDigit[number]);
+  strNumber = number.toString();
+  strNumberLenght = strNumber.length;
+if (0 <= number && number < 10){
+result = ones[number];
+} else if (10 <= number && number < 20){
+result = teens[number-10];
+}
+
+  console.log(strNumber);
+   console.log(strNumberLenght);
    return result;
   
  }
- let number = 4;
- toReadable(number);
+ /*
+ 14
+ 10
+ 22
+
+ */
+ let result;
+ let number = 6;
+ result = toReadable(number);
  
-  // console.log(result);
+   console.log(result);
