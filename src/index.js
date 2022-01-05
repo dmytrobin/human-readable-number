a128a51fa797020ce391f366326573a7864a0991
dmytrobin <dmytro.binyk@gmail.com>
2022-01-05 11:59:31 +0200

    fix: change hundreds function

--- src/index.js
+++ src/index.js
@@ -13,52 +13,53 @@ function ones(number){
   }
   
   function tens(number){
-  console.log(number);
   let numberTostring, strNumber;
     var  tens = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-     let strNumberLenght, secondDigit;
-    
+    let strNumberLenght, secondDigit; 
     strNumber = number.toString();
    // console.log(strNumber);
      if (strNumber[1]*1 === 0 ){
-     
-     numberTostring = tens[strNumber[0]*1-1];
-   } else { numberTostring = tens[strNumber[0]*1-1] + '\u00A0' + ones(strNumber[1]) ;
+        numberTostring = tens[strNumber[0]*1-1];
+      } else { numberTostring = tens[strNumber[0]*1-1]+' '+ones(strNumber[1]) ;
+//} else { numberTostring = tens[strNumber[0]*1-1] + '\u00A0' + ones(strNumber[1]) ;
    }
     
   return numberTostring;
   }
   
   function hundreds(number){
-   let numberTostring, strNumber, dd, d, zeroZero;
-   d = Math.floor(number/100);
-   dd = number - (Math.floor(number/100))*100;
-  
-   dd  === 0? zeroZero = '' : zeroZero=tens(dd);
-   numberTostring = ones(d) + '\u00A0' + 'hundred' + '\u00A0' + zeroZero;
-  
+    let numberTostring, strNumber, dd, d, zeroZero;
+    d = Math.floor(number/100);
+    dd = number - d * 100;
+    
+    dd  === 0? zeroZero = '' : zeroZero=tens(dd);
+    console.log(zeroZero);
+    numberTostring = ones(d)+' '+'hundred'+' '+zeroZero;
+		numberTostring = numberTostring.trim();
+    
    return numberTostring;
   }
   
-  function toReadable (number) {
+module.exports = function toReadable (number) {
     let result;
-   
-  //strNumberLenght = strNumber.length;
-  if (0 <= number && number < 10){
-  result = ones(number);
-  } else if (10 <= number && number < 20){
-  result = teens(number);
-  } else if (20 <= number && number < 100){
-   result = tens(number);
-   } else if   (100 <= number && number < 1000){
-   result = hundreds(number);
-   }
-   
-     return result; 
-   }//toReadable
+    console.log(number);
+    //strNumberLenght = strNumber.length;
+    if (0 <= number && number < 10){
+    result = ones(number);
+    } else if (10 <= number && number < 20){
+    result = teens(number);
+    } else if (20 <= number && number < 100){
+    result = tens(number);
+    } else if   (100 <= number && number < 1000){
+    result = hundreds(number);
+    }
+    
+      return result; 
+}//toReadable
+
+
   
    let result;
    let number = 965;
    //result = toReadable(number);
    
-  console.log(result);
