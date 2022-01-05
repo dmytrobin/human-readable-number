51c2607c094a569f36c38a3ca5fa13f2bb96cd7f
dmytrobin <dmytro.binyk@gmail.com>
2022-01-05 13:37:21 +0200

    fix: change hundreds function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function ones(number){
   let numberTostring;
-  var ones = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+  var ones = ['zero','one','two','three','four','five','six','seven','eight','nine', 'ten'];
   numberTostring = ones[number];
   return numberTostring;
   }
@@ -13,6 +13,7 @@ function ones(number){
   }
   
   function tens(number){
+  console.log(number);
   let numberTostring, strNumber;
     var  tens = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
     let strNumberLenght, secondDigit; 
@@ -28,21 +29,46 @@ function ones(number){
   }
   
   function hundreds(number){
-    let numberTostring, strNumber, dd, d, zeroZero;
-    d = Math.floor(number/100);
-    dd = number - d * 100;
+    let numberTostring, strNumber, hundreds, tensOrTeens, zeroZero;
+   	
+    strNumber = number.toString();
+		hundreds = strNumber[0]*1;
+    tensOrTeens = strNumber.substring(1)*1;
+    console.log(tensOrTeens);
     
+    if (1<=tensOrTeens && tensOrTeens<=10){
+    	tensOrTeens = ones(tensOrTeens);
+    } else if  (11<=tensOrTeens && tensOrTeens<=19){
+    tensOrTeens = teens(tensOrTeens);
+    } else if ( 20<=tensOrTeens && tensOrTeens<= 99){
+    tensOrTeens = tens(tensOrTeens);
+		} if (tensOrTeens == 0){
+    tensOrTeens = '';
+    }
+    
+	//	console.log(tensOrTeens);
+    numberTostring = ones(hundreds)+' '+'hundred'+' '+tensOrTeens;
+    numberTostring = numberTostring.trim();
+   // console.log(numberTostring);
+   
+   
+   /*
+    d = Math.floor(number/100);
+    dd = (number/100-d).toFixed(2)*100;
+    
+   // console.log(d);
+     console.log(dd);
     dd  === 0? zeroZero = '' : zeroZero=tens(dd);
     console.log(zeroZero);
     numberTostring = ones(d)+' '+'hundred'+' '+zeroZero;
 		numberTostring = numberTostring.trim();
-    
+  */
+
    return numberTostring;
   }
   
 module.exports = function toReadable (number) {
     let result;
-    console.log(number);
     //strNumberLenght = strNumber.length;
     if (0 <= number && number < 10){
     result = ones(number);
@@ -57,9 +83,13 @@ module.exports = function toReadable (number) {
       return result; 
 }//toReadable
 
+	//var startTime = window.performance.now();
 
   
    let result;
-   let number = 965;
+   let number = 900;
    //result = toReadable(number);
-   
+  // console.log(window.performance.now() - startTime);
+   console.log(result);
+
+
